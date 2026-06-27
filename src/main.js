e26fdcc036c90b9925ccd9ab41d37c27e700a0f0
sunxiao <sunxiao@soft.medrena.com>
2026-06-27 14:13:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
+import Slider from './components/Slider.js'
 import Summary from './components/Summary.js'
+import Archive from './components/Archive.js'
+import ContentMenu from './components/ContentMenu.js'
 
 import './css/main.scss'
 
 if ( document.getElementById("bookSummary") ) {
   new Summary()
+}
+
+if ( document.getElementById("sectionContainer") ) {
+  new Slider()
+}
+
+if ( document.getElementById("postArchive") ) {
+  new Archive()
+}
+
+if ( document.querySelector(".section-nav") ) {
+  new ContentMenu()
 }
